f4f85bfd2f339b90ba4ef486959bd8f9db250491
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-07-26 10:44:50 +0530

    Compute predictions from equations derived
    Numpy programmatic representation of Mathematic equations derived

--- linear_reg_impl.py
+++ linear_reg_impl.py
@@ -2,9 +2,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# load the data
 df = pd.read_csv('data_1d.csv', header = None)
 X = df[0].values
 Y = df[1].values
 
+# plot the data
 plt.scatter(X, Y)
+plt.show()
+
+# apply the equations we learned to calculate a and b
+# Numpy representation of Mathematic equations derived
+# denominator is common so computing once
+denominator = X.dot(X) - X.mean() * X.sum()
+a = ( X.dot(Y) - Y.mean() * X.sum() ) / denominator
+b = ( Y.mean() * X.dot(X) - X.mean() * X.dot(Y) ) / denominator
+
+# calculate the predicted Y
+Yhat = a * X + b
+
+# plot everything together to make sure it worked
+plt.scatter(X, Y)
+plt.plot(X, Yhat)
 plt.show()
