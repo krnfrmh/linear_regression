8da895b07d00e3d610b8e9868d4a639f8ec03d7a
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2021-10-11 23:44:35 +0530

    Gradient Descent weight updates

--- lr_gradient_descent.py
+++ lr_gradient_descent.py
@@ -10,3 +10,13 @@ X[:,0] = 1 # bias term
 X[:5,1] = 1
 X[5:,2] = 1
 Y = np.array([0]*5 + [1]*5)
+
+costs = []
+w = np.random.randn(D) / np.sqrt(D) # randomly initialize w
+learning_rate = 0.001
+for t in range(1000):
+  Yhat = X.dot(w)
+  delta = Yhat - Y
+  # weight update
+  w = w - learning_rate*X.T.dot(delta)
+  
